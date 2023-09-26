40f68dced07762e1176a16cdec008409fbbf419a
David Ludwig <davidludwigii@gmail.com>
2023-09-26 20:51:25 +0000

    Modify tf devices use for simpler setup

--- src/deepctx/integration/tensorflow/devices.py
+++ src/deepctx/integration/tensorflow/devices.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from typing import Optional, TypedDict
+from typing import Iterable, Optional, TypedDict
 
 from ...hardware import gpu_memory
 
@@ -38,18 +38,36 @@ def gpu_list() -> list[tf.config.PhysicalDevice]:
     return tf.config.list_physical_devices("GPU")
 
 
-# TODO: Swap main argument to cpus and gpus.
 def use(
-    cpus: Optional[list[tf.config.PhysicalDevice]] = None,
-    gpus: Optional[list[tf.config.PhysicalDevice]] = None,
+    *,
+    cpus: Optional[int|Iterable[tf.config.PhysicalDevice]|Iterable[int]|None] = ...,
+    gpus: Optional[int|Iterable[tf.config.PhysicalDevice]|Iterable[int]|None] = ...,
     use_dynamic_memory: bool = True
 ) -> list[tf.config.PhysicalDevice]:
     """
     Select the specified devices.
+
+    cpus: If given a number, the first n CPUs will be used. If given a list of devices/indices,
+          those devices will be used.
+    gpus: If given a number, the best n GPUs will be used. If given a list of devices/indices,
+          those devices will be used.
+    use_dynamic_memory: Use dynamic memory allocation.
     """
-    if cpus is not None:
+    if cpus is not Ellipsis:
+        if cpus is None:
+            cpus = []
+        elif isinstance(cpus, int):
+            cpus = cpu_list()[:cpus]
+        elif len(cpus) > 0 and isinstance(cpus[0], int):
+            cpus = [cpu_list()[i] for i in cpus]
         tf.config.set_visible_devices(cpus, "CPU")
-    if gpus is not None:
+    if gpus is not Ellipsis:
+        if gpus is None:
+            gpus = []
+        elif isinstance(gpus, int):
+            gpus = best_gpus(count=gpus)
+        elif len(gpus) > 0 and isinstance(gpus[0], int):
+            gpus = [gpu_list()[i] for i in gpus]
         tf.config.set_visible_devices(gpus, "GPU")
         for device in gpus:
             tf.config.experimental.set_memory_growth(device, use_dynamic_memory)
